80cdd717f6a7efd0f33c57061979ede6c9c9ed6c
Keane <drkeaned.dasilva@hotmail.com>
2016-11-17 09:25:18 -0500

    Relocated Sensitive info

---
+++ app/src/main/java/com/example/keane/popmovies/ApiKey.java
@@ -0,0 +1,12 @@
+/**
+ * Created by keane on 11/17/2016.
+ */
+package com.example.keane.popmovies;
+public class ApiKey {
+    private static final String API_KEY = "***REMOVED***";
+
+    public String get(){
+        return API_KEY;
+    }
+
+}
