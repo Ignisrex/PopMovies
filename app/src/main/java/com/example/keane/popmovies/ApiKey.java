f89cce7b6f01e80bb535375088a3b23bbe9524e1
Keane <drkeaned.dasilva@hotmail.com>
2016-11-17 21:31:51 -0500

    Add Apikey to git ignore

--- app/src/main/java/com/example/keane/popmovies/ApiKey.java
+++ app/src/main/java/com/example/keane/popmovies/ApiKey.java
@@ -3,7 +3,7 @@
  */
 package com.example.keane.popmovies;
 public class ApiKey {
-    private static final String API_KEY = "***REMOVED***";
+    private static final String API_KEY = "73a8157b5819018d96d349fc48b86ebc";
 
     public static String get(){
         return API_KEY;
