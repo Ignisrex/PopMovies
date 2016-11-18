23e830a285b53c506d3e3f6900b55b414e2886a5
Keane <drkeaned.dasilva@hotmail.com>
2016-11-17 22:05:55 -0500

    Sensitive date fix

---
+++ app/src/main/java/com/example/keane/popmovies/ApiKey.java
@@ -0,0 +1,12 @@
+/**
+ * Created by keane on 11/17/2016.
+ */
+package com.example.keane.popmovies;
+public class ApiKey {
+    private static final String API_KEY = "73a8157b5819018d96d349fc48b86ebc";
+
+    public static String get(){
+        return API_KEY;
+    }
+
+}
